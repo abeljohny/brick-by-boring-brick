6aa33d2b61f8466b7633ecaa9d66b536954ac526
Abel <abeljohny@users.noreply.github.com>
2025-09-25 00:34:34 +0100

    867

---
+++ 867.py
@@ -0,0 +1,7 @@
+class Solution:
+    def transpose(self, matrix: list[list[int]]) -> list[list[int]]:
+        return [[matrix[row][col] for row in range(len(matrix))] for col in range(len(matrix[0]))]
+
+
+# print(Solution().transpose([[1,2,3],[4,5,6],[7,8,9]]))
+print(Solution().transpose([[1,2,3],[4,5,6]]))
